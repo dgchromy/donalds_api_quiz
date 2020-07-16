2ed643f2658826f58908f9b206957ded0655feb6
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-07-15 22:28:28 -0700

    updated java script so save button works

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('score');
-const scoreText = document.getElementById ('progressBarFull');
+ const progressText = document.getElementById('progressText')
+ const scoreText = document.getElementById ('score')
+const progressBarFull = document.getElementById ('progressBarFull');
 
 
 let currentQuestion = {};
@@ -14,21 +15,21 @@ let questions = [
     {
         question: "What is a Array in javascript?",
         choice1: "it is a single variable to store different elements",
-        choice2: "a reference to ,ultiple variable",
+        choice2: "a reference to multiple variable",
         choice3: "a ray of sunshine",
         choice4: "A read only variable",
         answer: 1,
     },
     {
-        question: "what is the difference between JavaScript and Java code",
+        question: "what is the difference between JavaScript and Java code?",
         choice1: "Java is run on a web browser and JS is run in a virtual machine",
         choice2: "they are the same code",
-        choice3: "Java app are run virtual machine or web browser JS is run on a web browser",
+        choice3: "Java code are run on virtual machine or web browser JS is run on a web browser",
         choice4: "bears, beets, battlestar galatica!",
         answer: 3,
     },
     {
-        question: "how do you write an IF statement for executing some code IF i is NOT equal to 5" ,
+        question: "how do you write an IF statement for executing some code IF i is NOT equal to 5?" ,
         choice1: "if i <> 5",
         choice2: "if (i != 5)",
         choice3: "if (i <> 5 ",
@@ -42,7 +43,7 @@ var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 
 
-var secondsLeft = 45;
+var secondsLeft = 90;
 
 function setTime() {
     var timerInterval = setInterval(function() {
